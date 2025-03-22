1523fe8bd36e69df3bfa62bf05fd0ada2c78cdf1
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-22 23:46:07 +0600

    Refactor navigation and search functionality for improved user experience and layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,25 @@ searchBtn.onclick=function(){
     searchBox.classList.add('active')
     closeBtn.classList.add('active')
     searchBtn.classList.add('active')
+    menuToggle.classList.add('hide')
+    nav.classList.remove('open')
 }
 closeBtn.onclick=function(){
     searchBox.classList.remove('active')
     closeBtn.classList.remove('active')
     searchBtn.classList.remove('active')
+    menuToggle.classList.remove('hide')
 }
 
 // Search Button Functionality End
+
+let nav=document.querySelector('nav')
+let navigation=document.querySelector('.navigation')
+let menuToggle=document.querySelector('.menuToggle')
+
+menuToggle.onclick=function(){
+    nav.classList.toggle('open')
+    searchBox.classList.remove('active')
+    closeBtn.classList.remove('active')
+    searchBtn.classList.remove('active')
+}
