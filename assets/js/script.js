276b04cb1a4ab371037680a6776e0eea56899c9d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-22 22:02:58 +0600

    Enhance search functionality with button animations and visibility toggles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,11 @@ let closeBtn=document.querySelector('.closeBtn')
 let searchBox=document.querySelector('.searchBox')
 searchBtn.onclick=function(){
     searchBox.classList.add('active')
+    closeBtn.classList.add('active')
+    searchBtn.classList.add('active')
 }
 closeBtn.onclick=function(){
     searchBox.classList.remove('active')
+    closeBtn.classList.remove('active')
+    searchBtn.classList.remove('active')
 }
