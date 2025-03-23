bdcd2195e0136a85b92341d0147f23b9a7fc07f9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-23 16:30:56 +0600

    Remove unused dropdown functionality for mobile navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,26 +32,4 @@ menuToggle.onclick=function(){
     searchBtn.classList.remove('active')
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    let dropdowns = document.querySelectorAll(".dropdown > a");
-  
-    dropdowns.forEach((dropdown) => {
-      dropdown.addEventListener("click", (event) => {
-        if (window.innerWidth <= 770) {  // Only apply on small screens
-          event.preventDefault(); // Prevent the default link action
-  
-          let submenu = dropdown.nextElementSibling;
-          if (submenu.style.display === "block") {
-            submenu.style.display = "none";
-          } else {
-            // Hide all other open dropdowns
-            document.querySelectorAll(".dropdown ul").forEach((menu) => {
-              menu.style.display = "none";
-            });
-            submenu.style.display = "block";
-          }
-        }
-      });
-    });
-  });
   
